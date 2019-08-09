780bc94080d5a3831273f8d50817b7b96f7bed1f
MAC <nwokikesamson@gmail.com>
2019-08-09 14:14:23 +0100

    finish up on card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Board from './components/Board'
+import Card from './components/Card'
 
 function App() {
   return (
     <div className="App">
       <main className="flexbox">
-        Hre will be our board
+
+        <Board id="board-1" className="board">
+          <Card id="card-1" className="card" draggable="true">
+            <p>Card one</p>
+          </Card>
+        </Board>
+
+        <Board id="board-2" className="board">
+          <Card id="card-2" className="card" draggable="true">
+            <p>Card two</p>
+          </Card>
+        </Board>
 
       </main>
       
