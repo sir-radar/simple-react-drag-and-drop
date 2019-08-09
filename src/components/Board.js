348d06015b9aadc59aad5d795f4a5948ed22eb88
MAC <nwokikesamson@gmail.com>
2019-08-09 12:25:32 +0100

    finish up on board component

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,13 +1,28 @@
 import React from 'react'
 
 function Board(props) {
-  const drop = e =>{
+  const drop = e => {
     e.preventDefault();
-    const card_id = e.dataTransfer.getData('card_id')
+    const card_id = e.dataTransfer.getData('card_id');
+
+    const card = document.getElementById(card_id);
+    card.style.display = 'block';
+
+    e.target.appendChild(card); 
+  }
+
+  const dragOver = e => {
+    e.preventDefault();
+
   }
 
   return (
-    <div>
+    <div
+      id={props.id}
+      className={props.className}
+      onDrop={drop}
+      onDragOver={dragOver}
+    >
       {props.children}
     </div>
   )
